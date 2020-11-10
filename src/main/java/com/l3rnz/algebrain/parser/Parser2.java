82d9278a5f70951a15e5c9974eef324d9bbf5c6e
Kyle <aretae@gmail.com>
2020-11-09 22:08:48 -0700

    Address subtract

--- src/main/java/com/l3rnz/algebrain/parser/Parser2.java
+++ src/main/java/com/l3rnz/algebrain/parser/Parser2.java
@@ -4,11 +4,17 @@ import com.l3rnz.algebrain.domain.*;
 import com.l3rnz.algebrain.exception.ExpressionException;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Parser2 {
 
     public static final Token MULTIPLY_TOKEN = new Token("*");
+    public static final Token ADD_TOKEN = new Token("+");
+    public static final Token SUBTRACT_TOKEN = new Token("-");
+    public static final Token [] TOKENS_THAT_PRECURSE_NEGATIVE = new Token[] {
+            MULTIPLY_TOKEN, ADD_TOKEN, SUBTRACT_TOKEN
+    };
 
     enum TokenType {
         TERM(1),
@@ -60,7 +66,9 @@ public class Parser2 {
 
     public Expression parse(List<Token> tokens) {
         Expression returnExpression;
-        if (isProduct(tokens)) {
+        if (isSum(tokens)) {
+            returnExpression = parseSum(tokens);
+        } else if (isProduct(tokens)) {
             returnExpression = parseProduct(tokens);
         } else {
             returnExpression = processTermAndNegative(tokens);
@@ -68,8 +76,77 @@ public class Parser2 {
         return returnExpression;
     }
 
+    private Expression parseSum(List<Token> tokens) {
+        Sum sum= new Sum();
+        List<Token> currentTokenList = tokens;
+        int indexOfSumSeparator;
+        List<Token> subList = null;
+
+        boolean explicit=true;
+        int modifier;
+        while (isSum(currentTokenList)) {
+            indexOfSumSeparator = findFirstSplitIndex(currentTokenList);
+            subList = currentTokenList.subList(0, indexOfSumSeparator);
+            sum.addExpression(parse(subList),explicit);
+            if (currentTokenList.get(indexOfSumSeparator).equals(SUBTRACT_TOKEN)) {
+                modifier=0;
+                explicit = false;
+            } else {
+                modifier = 1;
+                explicit = true;
+            }
+            currentTokenList = currentTokenList.subList(indexOfSumSeparator+modifier, currentTokenList.size());
+        }
+        sum.addExpression(parse(currentTokenList), explicit);
+        return sum;
+    }
+
+    private int findFirstSplitIndex(List<Token> currentTokenList) {
+        int addIndex = currentTokenList.indexOf(ADD_TOKEN);
+        int subtractIndex = findSubtractIndex(currentTokenList);
+        int index = 0;
+        if (subtractIndex < 0 || (addIndex >= 0 && addIndex < subtractIndex)) {
+            index = addIndex;
+        } else {
+            index = subtractIndex;
+        }
+        return index;
+    }
+
+    private int findSubtractIndex(List<Token> currentTokenList) {
+        Token token;
+        for (int i = 1; i<currentTokenList.size(); i++) {
+            token = currentTokenList.get(i);
+            if (token.equals(SUBTRACT_TOKEN) && isNotJustNegative(currentTokenList,i)){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    private boolean isSum(List<Token> tokens) {
+        return tokens.contains(ADD_TOKEN)
+                || isDifference(tokens);
+    }
+
+    private boolean isDifference(List<Token> tokens) {
+        if (! tokens.contains(SUBTRACT_TOKEN)) {
+            return false;
+        }
+        for(int index = 1; index < tokens.size(); index++) {
+            if (SUBTRACT_TOKEN.equals(tokens.get(index))
+                    && isNotJustNegative(tokens, index)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean isNotJustNegative(List<Token> tokens, int index) {
+        return !Arrays.asList(TOKENS_THAT_PRECURSE_NEGATIVE).contains(tokens.get(index-1));
+    }
+
     Expression parseProduct(List<Token> tokens) {
-        Expression returnExpression;
         Product product = new Product();
         List<Token> currentTokenList = tokens;
         int indexOfMultiply;
@@ -82,8 +159,7 @@ public class Parser2 {
             currentTokenList = currentTokenList.subList(indexOfMultiply+1, currentTokenList.size());
         }
         product.addExpression(parse(currentTokenList));
-        returnExpression = product;
-        return returnExpression;
+        return product;
     }
 
     private boolean isProduct(List<Token> tokens) {
